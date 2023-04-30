2d938d2f263a3cd0622db1e17d0706f5d143e2c8
Tsimafei Petrykevich <petrik3003@gmail.com>
2023-04-30 21:51:31 +0300

    feat: window manager render

--- includes/window_manager.h
+++ includes/window_manager.h
@@ -14,6 +14,7 @@ class WindowManager {
     void CloseWindow();
 
    private:
+    WINDOW nwindow_;
     unique_ptr<Window> current_window_;
     unique_ptr<Window> previous_window_;
 };
--- src/window_manager.cc
+++ src/window_manager.cc
@@ -16,24 +16,25 @@ void WindowManager::Update() {
         return;
     }
     clear();
-    current_window_->Render();
-    current_window_->HandleInput();
+    current_window_->Render(nwindow_);
+    int ch = getch();
+    current_window_->HandleInput(ch);
     refresh();
 }
 
 void WindowManager::ChangeWindow(unique_ptr<Window> next_window) {
-    previous_window_ = move(current_window_);
-    current_window_ = move(next_window);
+    previous_window_ = std::move(current_window_);
+    current_window_ = std::move(next_window);
 }
 
 void WindowManager::ReturnToPreviousWindow() {
     if (!previous_window_) {
         return;
     }
-    current_window_ = move(previous_window_);
+    current_window_ = std::move(previous_window_);
 }
 
 void WindowManager::CloseWindow() {
-    previous_window_ = move(current_window_);
+    previous_window_ = std::move(current_window_);
 }
 }  // namespace kittens
