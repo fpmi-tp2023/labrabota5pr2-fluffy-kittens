30ae6361c9f4e44f541bc0f9aa2e3840a0cd38e6
Tsimafei Petrykevich <petrik3003@gmail.com>
2023-06-04 02:02:52 +0300

    ref: win manager use shared ptr

--- includes/window_manager.h
+++ includes/window_manager.h
@@ -9,8 +9,8 @@ class WindowManager {
    public:
     static WindowManager* Instance();
     static void CleanUp();
+    void ChangeWindow(shared_ptr<Window> next_window);
     void Update();
-    void ChangeWindow(unique_ptr<Window> next_window);
     void ReturnToPreviousWindow();
     void CloseWindow();
     bool WindowActive() const;
@@ -19,8 +19,8 @@ class WindowManager {
     static WindowManager* instance_;
     WindowManager();
     ~WindowManager();
-    unique_ptr<Window> current_window_;
-    unique_ptr<Window> previous_window_;
+    shared_ptr<Window> current_window_;
+    shared_ptr<Window> previous_window_;  // TO DO: Change to Stack
 };
 }  // namespace kittens
 
