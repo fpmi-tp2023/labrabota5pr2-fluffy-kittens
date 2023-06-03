4db7207da0841ebb318b7571b3e5b878bc352488
Tsimafei Petrykevich <petrik3003@gmail.com>
2023-06-04 02:03:16 +0300

    feat: use window creation in main

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,6 @@
 #include "../includes/nmenu.h"
 #include "../includes/types.h"
+#include "../includes/window_creators.h"
 #include "../includes/window_manager.h"
 
 using namespace kittens;
@@ -7,14 +8,9 @@ using namespace kittens;
 int main() {
     WindowManager& win_manager = *WindowManager::Instance();
     atexit(WindowManager::CleanUp);
-    Menu menu{"Main"};
-    menu.AddItem("Login", [] { return; });
-    menu.AddItem("Sign Up", [] { return; });
-    menu.AddItem("Exit", [] { WindowManager::Instance()->CloseWindow(); });
-    win_manager.ChangeWindow(make_unique<Menu>(menu));
+    win_manager.ChangeWindow(CreateMainMenu());
     while (win_manager.WindowActive()) {
         win_manager.Update();
     }
-
     return 0;
 }
