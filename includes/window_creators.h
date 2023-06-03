427870f6481c53f44ed4b86655d74cf716a819eb
Tsimafei Petrykevich <petrik3003@gmail.com>
2023-06-04 02:02:40 +0300

    feat: window creation methods

---
+++ includes/window_creators.h
@@ -0,0 +1,14 @@
+#ifndef INCLUDES_WINDOW_CREATORS_H_
+#define INCLUDES_WINDOW_CREATORS_H_
+
+#include "./nform.h"
+#include "./nmenu.h"
+
+namespace kittens {
+shared_ptr<Menu> CreateMainMenu();
+shared_ptr<Form> CreateSignUpForm();
+shared_ptr<Menu> CreateAuthenthicationMenu();
+shared_ptr<Form> CreateLoginForm();
+}  // namespace kittens
+
+#endif /* INCLUDES_WINDOW_CREATORS_H_ */
