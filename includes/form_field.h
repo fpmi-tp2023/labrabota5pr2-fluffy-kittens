61b0ce035980b6ece9c680b7af4eb61d7cc06e7e
Tsimafei Petrykevich <petrik3003@gmail.com>
2023-06-03 21:52:34 +0300

    feat: form field

---
+++ includes/form_field.h
@@ -0,0 +1,27 @@
+#ifndef INCLUDES_FORM_FIELD_H_
+#define INCLUDES_FORM_FIELD_H_
+
+#include <ncurses.h>
+
+#include "./types.h"
+
+namespace kittens {
+class FormField {
+   public:
+    FormField(string label, function<bool(string)> validator, int max_len = 20);
+    virtual void HandleInput(int ch);
+    void Erase();
+    virtual void Render(WINDOW* window, int x, int y) const;
+    bool Validate() const;
+    string_view GetValue() const;
+    string_view GetLabel() const;
+    int GetMaxLen() const;
+
+   protected:
+    string label_;
+    function<bool(string)> validator_;
+    string value_;
+    int max_len_;
+};
+}  // namespace kittens
+#endif /* INCLUDES_FORM_FIELD_H_ */
