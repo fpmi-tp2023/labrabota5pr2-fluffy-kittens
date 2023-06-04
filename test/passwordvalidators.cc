4fd976ab878875fee2c0c85c7edff61482593e70
Artyom Shpakovski <artyom.shpkvs@gmail.com>
2023-06-04 22:58:54 +0300

    developed 15 tests

--- test/passwordvalidators.cc
+++ test/passwordvalidators.cc
@@ -22,3 +22,63 @@ TEST(PasswordValidation, SimpleString4) {
 TEST(PasswordValidation, SimpleString5) {
     EXPECT_EQ(kittens::IsPasswordValid("fghjkoiuytews"), true);
 }
+
+TEST(PasswordValidation, NotEnoghLenth1) {
+    EXPECT_EQ(kittens::IsPasswordValid("1"), false);
+}
+
+TEST(PasswordValidation, NotEnoghLenth2) {
+    EXPECT_EQ(kittens::IsPasswordValid("sad"), false);
+}
+
+TEST(PasswordValidation, NotEnoghLenth3) {
+    EXPECT_EQ(kittens::IsPasswordValid("asdsd"), false);
+}
+
+TEST(PasswordValidation, NotEnoghLenth4) {
+    EXPECT_EQ(kittens::IsPasswordValid(""), false);
+}
+
+TEST(PasswordValidation, NotEnoghLenth5) {
+    EXPECT_EQ(kittens::IsPasswordValid("123"), false);
+}
+
+TEST(PasswordValidation, TooBigLength1) {
+    EXPECT_EQ(kittens::IsPasswordValid("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"), false);
+}
+
+TEST(PasswordValidation, TooBigLength2) {
+    EXPECT_EQ(kittens::IsPasswordValid("1234567890iuygtfdsazxcvbnhjuytrewq2345"), false);
+}
+
+TEST(PasswordValidation, TooBigLength3) {
+    EXPECT_EQ(kittens::IsPasswordValid("asdfghjkliuytrew234rtfdsaASDFGHFDWER434567"), false);
+}
+
+TEST(PasswordValidation, TooBigLength4) {
+    EXPECT_EQ(kittens::IsPasswordValid("12345678909876543212345678908765432123456789098765432"), false);
+}
+
+TEST(PasswordValidation, TooBigLength5) {
+    EXPECT_EQ(kittens::IsPasswordValid("sdfghjuytrewsaxcvbhjytrewsxcvbnhjuytresxcvbnhjuytredcvbhytred"), false);
+}
+
+TEST(PasswordValidation, Spaces1) {
+    EXPECT_EQ(kittens::IsPasswordValid("          "), false);
+}
+
+TEST(PasswordValidation, Spaces2) {
+    EXPECT_EQ(kittens::IsPasswordValid("asdsa 123123"), false);
+}
+
+TEST(PasswordValidation, Spaces3) {
+    EXPECT_EQ(kittens::IsPasswordValid("1232132211 "), false);
+}
+
+TEST(PasswordValidation, Spaces4) {
+    EXPECT_EQ(kittens::IsPasswordValid("616326132613261 32 "), false);
+}
+
+TEST(PasswordValidation, Spaces5) {
+    EXPECT_EQ(kittens::IsPasswordValid("23456hvde 3456789oijhgfde3456yh"), false);
+}
